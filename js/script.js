01a271cb7521a02308761cffa5610c995586c7ef
Dan <daniel.munozdev@gmail.com>
2017-10-08 17:17:44 -0700

    Add p after .well div with after when a button is clicked

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@ const globalScopeVariable = 'I\'m a global scoped variable';
 
 $('a#click-one').click(function() {
   const localScopeVariable = 'I\'m a local scoped variable!';
+  $('.well').after('<p><code>const localScopeVariable = \'' + localScopeVariable +'\'; &amp; const globalScopeVariable = \'' + globalScopeVariable +'\';</code><p>');
   alert(globalScopeVariable);
   alert(localScopeVariable);
 });
 
 $('a#click-two').click(function(){
+  $('.well').after('<p><code>const localScopeVariable = \'' + typeof localScopeVariable +'\'; &amp; const globalScopeVariable = \'' + globalScopeVariable +'\';</code><p>');
   alert(globalScopeVariable);
   alert(localScopeVariable);
 });
