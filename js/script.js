96862270d037fca2ab7789ebca7270789d82bb30
Dan <daniel.munozdev@gmail.com>
2017-10-08 17:18:40 -0700

    Remove p when p is clicked

--- js/script.js
+++ js/script.js
@@ -3,12 +3,22 @@ const globalScopeVariable = 'I\'m a global scoped variable';
 $('a#click-one').click(function() {
   const localScopeVariable = 'I\'m a local scoped variable!';
   $('.well').after('<p><code>const localScopeVariable = \'' + localScopeVariable +'\'; &amp; const globalScopeVariable = \'' + globalScopeVariable +'\';</code><p>');
+  $('p').first().click(function() {
+    $(this).slideUp(300, function() {
+      $(this).remove();
+    });
+  });
   alert(globalScopeVariable);
   alert(localScopeVariable);
 });
 
 $('a#click-two').click(function(){
   $('.well').after('<p><code>const localScopeVariable = \'' + typeof localScopeVariable +'\'; &amp; const globalScopeVariable = \'' + globalScopeVariable +'\';</code><p>');
+  $('p').first().click(function() {
+    $(this).fadeOut(300, function() {
+      $(this).remove();
+    });
+  });
   alert(globalScopeVariable);
   alert(localScopeVariable);
 });
